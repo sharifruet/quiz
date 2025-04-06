6f4c740e8eae60bc52a96f55e8455f3b83ca2481
Sharif <sharifruet@gmail.com>
2025-04-06 18:20:44 +0600

    saved

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -4,12 +4,8 @@ import './Logo.css';
 export default function Logo() {
     return (
       <div className="logo-container">
-        <svg className="logo-icon" viewBox="0 0 40 40">
-          <circle cx="20" cy="20" r="18" fill="url(#logoGradient)"/>
-          <circle cx="20" cy="20" r="12" fill="white"/>
-          <circle cx="20" cy="20" r="6" fill="#FFCC00"/>
-        </svg>
-        <span className="logo-text">uizTime</span>
+        <img className="logo-icon" src = "/logo.png"/>
+        <span className="logo-text">QuizTime</span>
       </div>
     );
   }
